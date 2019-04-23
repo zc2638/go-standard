3ba236bd77e4bae86006e83dc434271d31ae76ca
zc <zc2638@qq.com>
2019-04-23 10:16:37 +0800

    增加fmt格式化类型，增加字符串倒序demo

--- src/strings/example.go
+++ src/strings/example.go
@@ -10,6 +10,10 @@ import (
 func main() {
 
 	strDemo()
+
+	// 字符串倒序
+	s := reverse("Hello, World!")
+	fmt.Println(s)
 }
 
 func strDemo() {
@@ -164,4 +168,12 @@ func strDemo() {
 			return 'a'
 		}
 	}, a)
+}
+
+func reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
