c4d39e1eb40c7f64931fc0a11067b3f05cce3adc
zc <zc2638@qq.com>
2019-09-06 15:33:30 +0800

    errors update custom

--- src/errors/example.go
+++ src/errors/example.go
@@ -9,6 +9,7 @@ type CustomError string
 
 func (e CustomError) Error() string { return string(e) }
 func (e CustomError) Unwrap() error { return errors.New("unwrap error") }
+func (e CustomError) As(err error) bool { return true }
 
 func main() {
 
@@ -16,11 +17,11 @@ func main() {
 	err := errors.New("test error")
 	fmt.Println(err)
 
-	ce := CustomError("test error")
+	ce := CustomError("test error1")
 	fmt.Println(errors.Is(err, ce))
 
 	var e error
-	fmt.Println(errors.As(err, &e), e)
+	fmt.Println(errors.As(ce, &e), e)
 
 	fmt.Println(errors.Unwrap(ce))
 }
