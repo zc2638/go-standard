51ec8886163005b7733d7bcde6fb732bb2a0b925
zc <zc2638@qq.com>
2019-09-06 15:26:51 +0800

    add errors package new func

--- src/errors/example.go
+++ src/errors/example.go
@@ -5,9 +5,22 @@ import (
 	"fmt"
 )
 
+type CustomError string
+
+func (e CustomError) Error() string { return string(e) }
+func (e CustomError) Unwrap() error { return errors.New("unwrap error") }
+
 func main() {
 
 	// 新建error
 	err := errors.New("test error")
 	fmt.Println(err)
+
+	ce := CustomError("test error")
+	fmt.Println(errors.Is(err, ce))
+
+	var e error
+	fmt.Println(errors.As(err, &e), e)
+
+	fmt.Println(errors.Unwrap(ce))
 }
